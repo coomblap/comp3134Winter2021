181ac539d4edf00bb9730b16e08bb64b3cd26eb8
Thinh Tran <thinhtran1769@gmail.com>
2021-04-18 23:23:28 -0400

    Files were added

---
+++ wk12/csfr.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"></head><body data-new-gr-c-s-check-loaded="14.1006.0" data-gr-ext-installed="">
+<form action="http://134.122.47.167/csfr.php#" method="post">
+    <p>Username: <input type="text" name="username"></p>
+    <p>Password: <input type="text" name="password"></p>
+    <p><input type="submit" value="Submit"></p>
+</form>
+<div>
+    </div>
+</body></html>
